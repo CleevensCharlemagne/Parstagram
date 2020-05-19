5abdcc95e9c5427b6b062cc773d4c366efa121c1
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2020-05-19 19:42:58 -0400

    Isue, profile page is still blank

---
+++ app/src/main/java/fragment/ProfileFragment.java
@@ -0,0 +1,44 @@
+package fragment;
+
+import android.util.Log;
+
+import com.example.parstagram.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends HomeFragment {
+
+    private static final String TAG = "ProfileFragment";
+
+    @Override
+    protected void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.addDescendingOrder(Post.KEY_CREATED_AT);
+
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e != null){
+                    Log.e(TAG, "Issue with getiing posts, e");
+                    return;
+                }
+
+                for(Post post : posts){
+                    Log.i(TAG, "Post : " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
